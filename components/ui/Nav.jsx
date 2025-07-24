a412c8b0d9c47a0cf8f32a9176b94134900e7751
Andy Mai <112735674+amai-21@users.noreply.github.com>
2025-07-24 10:54:44 -0500

    Made it so that clicking on a link will make the link have a cyan highlight to indicate that the user is within that page.

--- components/ui/Nav.jsx
+++ components/ui/Nav.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import Link from "next/Link";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
 
 const links = [
@@ -23,12 +23,19 @@ const links = [
 ];
 
 const Nav = () => {
+    const pathname = usePathname();
+    console.log(pathname);
+
     return (
         <nav className = "flex gap-8">
             {links.map((link, index) => {
-                return <Link href = {link.path} key = {index}>
-                        {link.name}
-                    </Link>
+                return ( 
+                <Link href = {link.path} key = {index} className = {`${link.path === pathname && "text-accent border-b-2 border-accent"
+                }`}>
+                
+                {link.name}
+                </Link>
+            );
             })}
         </nav>
     );
